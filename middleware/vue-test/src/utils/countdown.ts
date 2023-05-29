349c6267c8b948c6186aa05313cc01d12e0d0866
Frank Weijers <frank@weijers.io>
2023-05-29 23:48:10 +0200

    Fix countdown text

--- middleware/vue-test/src/utils/countdown.ts
+++ middleware/vue-test/src/utils/countdown.ts
@@ -1,33 +1,30 @@
 export function countdown(countDownToTime: number) {
-  const timeNow = new Date().getTime();
-  const timeDifference = countDownToTime - timeNow;
-  const millisecondsInOneSecond = 1000;
-  const millisecondsInOneMinute = millisecondsInOneSecond * 60;
-  const millisecondsInOneHour = millisecondsInOneMinute * 60;
-  const millisecondsInOneDay = millisecondsInOneHour * 24;
-  const differenceInDays = timeDifference / millisecondsInOneDay;
-  const remainderDifferenceInHours =
-    (timeDifference % millisecondsInOneDay) / millisecondsInOneHour;
-  const remainderDifferenceInMinutes =
-    (timeDifference % millisecondsInOneHour) / millisecondsInOneMinute;
-  const remainderDifferenceInSeconds =
-    (timeDifference % millisecondsInOneMinute) / millisecondsInOneSecond;
-  const remainingDays = Math.floor(differenceInDays);
-  const remainingHours = Math.floor(remainderDifferenceInHours);
-  const remainingMinutes = Math.floor(remainderDifferenceInMinutes);
-  const remainingSeconds = Math.floor(remainderDifferenceInSeconds);
+  let difference = Math.floor(Date.now() / 1000) - Math.floor(countDownToTime / 1000);
+  let output = ``;
 
-  return (
-    remainingDays +
-    ' Days ' +
-    ': ' +
-    remainingHours +
-    ' Hours ' +
-    ': ' +
-    remainingMinutes +
-    ' Minutes ' +
-    ': ' +
-    remainingSeconds +
-    ' Seconds'
-  );
+  if (difference < 0) {
+    difference = difference - difference - difference;
+  }
+
+  if (difference < 60) {
+    // Less than a minute has passed:
+    output = `${difference} seconds`;
+  } else if (difference < 3600) {
+    // Less than an hour has passed:
+    output = `${Math.floor(difference / 60)} minutes`;
+  } else if (difference < 86400) {
+    // Less than a day has passed:
+    output = `${Math.floor(difference / 3600)} hours`;
+  } else if (difference < 2620800) {
+    // Less than a month has passed:
+    output = `${Math.floor(difference / 86400)} days`;
+  } else if (difference < 31449600) {
+    // Less than a year has passed:
+    output = `${Math.floor(difference / 2620800)} months`;
+  } else {
+    // More than a year has passed:
+    output = `${Math.floor(difference / 31449600)} years`;
+  }
+
+  return output;
 }
