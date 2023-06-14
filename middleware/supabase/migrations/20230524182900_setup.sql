2cdb71e7aa19cf919688fd23d1b34ac5f9ffac7f
Frank Weijers <frank@weijers.io>
2023-06-14 22:41:35 +0200

    Enable pg_cron + pg_net extensions

--- middleware/supabase/migrations/20230524182900_setup.sql
+++ middleware/supabase/migrations/20230524182900_setup.sql
@@ -1,2 +1,6 @@
+-- Enable the cron + pg_net extensions
+create extension if not exists pg_cron;
+create extension if not exists pg_net;
+
 -- Convert snake_case (postgres) names to camelCase (GraphQL)
 comment on schema public is '@graphql({"inflect_names": true})';
